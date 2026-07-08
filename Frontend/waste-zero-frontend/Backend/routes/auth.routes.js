4ce6138a2e25199756b50d04c5f0a3b2be6f7a77
Vaishnavi <shrivastavavaishnavi77@gmail.com>
2026-07-08 11:54:57 +0530

    Remove duplicate backend from Angular project

--- Frontend/waste-zero-frontend/Backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/auth.controllers');
-
-// Map fields directly to the controllers
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-
-module.exports = router;
