847b1863807d86060137d3e30cddb929bbe91082
Dhruv Pathak <dhruvpathak1375@gmail.com>
2026-08-05 22:44:06 +0530

    Delete ui_fixes directory

--- ui_fixes/Backend/utils/passwordValidator.js
+++
@@ -1,7 +0,0 @@
-const passwordValidator = (password) => {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-  return regex.test(password);
-};
-
-module.exports = passwordValidator;
